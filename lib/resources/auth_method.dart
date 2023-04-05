9ab22d737b65065b8b1a3da70cd9517a8a95c991
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-04-05 08:27:05 +0100

    added the firebase authentication and snackbar

--- lib/resources/auth_method.dart
+++ lib/resources/auth_method.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:zoom_clone_app/utili/utili.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -34,8 +35,10 @@ class AuthMethods {
         }
         res = true;
       }
-    } catch (e) {
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!);
       res = false;
     }
+    return res;
   }
 }
